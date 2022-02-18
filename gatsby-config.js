80c12cd8ed9e60488f41b15cc1fd193d8522b073
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2022-02-18 14:56:45 +0300

    update sineUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Алексей Горбунов. Ведущий. Шоумен. +7 985 644 64 89`,
     description: `Алексей Горбунов. Ведущий праздничных мероприятий любого формата. Веду свадьбы, корпоративы, дни рождения, детские и городские праздники и т.д. +7 985 644 64 89`,
     author: `marsel.gabdulov@gmail.com`,
-    siteUrl: `marsdev.ru`,
+    siteUrl: `https://alexeygorbunov.com`,
   },
   plugins: [
     `gatsby-plugin-sass`,
