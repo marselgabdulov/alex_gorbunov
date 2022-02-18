1ceb7ee14c0e239302cf1f3463eedd3631715db2
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2022-02-18 13:34:51 +0300

    add empty videos page

---
+++ src/pages/videos.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import "./styles/videos.scss"
+
+const VideosPage = () => {
+  return (
+    <Layout>
+      <Seo title="Видео | Ведущий Алексей Горбунов. +7 985 644 64 89" />
+      <div className="videos-page">
+        <h1>Мои видео:</h1>
+        <p>Страница в разработке</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default VideosPage
