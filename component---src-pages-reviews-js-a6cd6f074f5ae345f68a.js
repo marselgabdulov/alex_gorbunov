9c4c08989fde59e1992bece6f7f5f71efe2f75dd
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2022-02-19 07:42:43 +0300

    Updates

---
+++ component---src-pages-reviews-js-a6cd6f074f5ae345f68a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[303],{6899:function(e,t,a){a.r(t);var l=a(7294),n=a(293),r=a(9357);t.default=function(){return l.createElement(n.Z,null,l.createElement(r.Z,{title:"Отзывы | Ведущий Алексей Горбунов. +7 985 644 64 89"}),l.createElement("div",{className:"reviews-page"},l.createElement("h1",null,"Отзывы:"),l.createElement("p",null,"Страница в разработке")))}}}]);
+//# sourceMappingURL=component---src-pages-reviews-js-a6cd6f074f5ae345f68a.js.map
--- component---src-pages-videos-js-eb16278ddf537458275d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[58],{7017:function(e,t,a){a.r(t);var l=a(7294),n=a(7283),r=a(9357);t.default=function(){return l.createElement(n.Z,null,l.createElement(r.Z,{title:"Видео | Ведущий Алексей Горбунов. +7 985 644 64 89"}),l.createElement("div",{className:"videos-page"},l.createElement("h1",null,"Мои видео:"),l.createElement("p",null,"Страница в разработке")))}}}]);
-//# sourceMappingURL=component---src-pages-videos-js-eb16278ddf537458275d.js.map
